549897d1c1027b94b133e2fdc19358d1710141a6
samapikart <31254492+samapikart@users.noreply.github.com>
2017-08-31 00:47:55 +0530

    Create Prob 4.cpp

---
+++ Prob 4.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	
+ float d, w, y;
+ cout<< "Enter number of days :";
+ 
+ cin >>d;
+ w=d/7;
+ y=d/365;
+ cout << "The number of weeks is "<<w<<endl<<"The number of years is "<<y;
+	return 0;
+}
