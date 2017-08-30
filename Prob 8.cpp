7cc3f9b17739857700986718901fb547ed4c247c
samapikart <31254492+samapikart@users.noreply.github.com>
2017-08-31 00:50:50 +0530

    Create Prob 8.cpp

---
+++ Prob 8.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+ float b,h,a;
+ cout<< "Enter base and height of a triangle :";
+ 
+ cin >>b>>h;
+ a=(0.5*b)*h;
+ 
+ cout << "Area of the triangle is " << endl << a;
+	return 0;
+}
