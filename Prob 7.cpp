d0582db7b7b1102e6eef786bef5d17361d05d769
samapikart <31254492+samapikart@users.noreply.github.com>
2017-08-31 00:49:47 +0530

    Create Prob 7.cpp

---
+++ Prob 7.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+ float a,b,c;
+ cout<< "Enter two angles of a triangle :";
+ 
+ cin >>a>>b;
+ c=180-(a+b);
+ 
+ cout << "The third angle is" << endl << c;
+	return 0;
+}
