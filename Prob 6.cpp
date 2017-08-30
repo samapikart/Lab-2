b607580d312d1e5d325aa85f2368056c699176df
samapikart <31254492+samapikart@users.noreply.github.com>
2017-08-31 00:48:59 +0530

    Create Prob 6.cpp

---
+++ Prob 6.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main() {
+	
+ float x, sq;
+ 
+ cin >> x;
+ sq= pow(x,0.5);
+ 
+ cout << "The square root of any number x is" << endl << sq;
+	return 0;
+}
