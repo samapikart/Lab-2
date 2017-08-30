98e0e29a24602a28a7839acada3a4d7d774cc9f4
samapikart <31254492+samapikart@users.noreply.github.com>
2017-08-31 00:44:12 +0530

    Create Prob 2.cpp

---
+++ Prob 2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+ float C,F;
+ 
+ cout << "Enter temperature in Celsius:" << endl;
+ cin >> C;
+ F=(C*9/5)+32;
+ cout << "Temperature in Fahrenheit is" << endl << F;
+	return 0;
+}
