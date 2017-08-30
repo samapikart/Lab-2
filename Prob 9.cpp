88840bb3ff9099572baa0ee93df9d1d5b70fb910
samapikart <31254492+samapikart@users.noreply.github.com>
2017-08-31 00:52:50 +0530

    Create Prob 9.cpp

---
+++ Prob 9.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+ float l,a;
+ cout<< "Enter length of the side of a triangle :";
+ 
+ cin >>l;
+ a=1.732/4*l*l;
+ 
+ cout << "Area of the triangle is " << endl << a;
+	return 0;
+}
