250c680df8cbd2fb78bb57ad7f45b01328bd4826
samapikart <31254492+samapikart@users.noreply.github.com>
2017-08-31 00:48:29 +0530

    Create Prob 5.cpp

---
+++ Prob 5.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main() {
+	
+ float x, y, p;
+ 
+ cin >> x >>y;
+ p= pow(x,y);
+ 
+ cout << "The power of any number x^y is" << endl << p;
+	return 0;
+}
